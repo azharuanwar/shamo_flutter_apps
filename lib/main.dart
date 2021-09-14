48fe2898b6d96e5e87471ab5f62719bc3c18a390
Azharu Anwar <azharu.anwar@gmail.com>
2021-09-14 22:26:56 +0700

    gatau:

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shamo_apps/pages/cart_page.dart';
+
 import 'package:shamo_apps/pages/checkout_page.dart';
 import 'package:shamo_apps/pages/detail_chat.dart';
 import 'package:shamo_apps/pages/edit_profile.dart';
 import 'package:shamo_apps/pages/home/main_page.dart';
-import 'package:shamo_apps/pages/product_page.dart';
+// import 'package:shamo_apps/pages/product_page.dart';
 import 'package:shamo_apps/pages/signin_page.dart';
 import 'package:shamo_apps/pages/signup_page.dart';
 import 'package:shamo_apps/pages/splash_page.dart';
@@ -13,14 +14,14 @@ import 'package:shamo_apps/pages/success_page.dart';
 import 'package:shamo_apps/providers/auth_provider.dart';
 import 'package:shamo_apps/providers/product_provider.dart';
 import 'package:shamo_apps/providers/wishlist_provider.dart';
-import 'package:shamo_apps/services/product_service.dart';
+// import 'package:shamo_apps/services/product_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ProductService().getProducts().then((value) => print("value: $value"));
+    // ProductService().getProducts().then((value) => print("value: $value"));
 
     return MultiProvider(
       providers: [
