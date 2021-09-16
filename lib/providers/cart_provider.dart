5f3e926a92b96eeed6f6ef2c39a3977f9105726f
Azharu Anwar <azharu.anwar@gmail.com>
2021-09-16 21:24:32 +0700

    commit

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -12,12 +12,59 @@ class CartProvider with ChangeNotifier {
   }
 
   addCart(ProductModel product) {
+    if (productExist(product)) {
+      int idx = _carts.indexWhere((element) => element.product?.id == product.id);
+      _carts[idx].qty = (_carts[idx].qty ?? 0) + 1;
+    } else {
+      _carts.add(CartModel(
+        id: _carts.length,
+        product: product,
+        qty: 1,
+      ));
+    }
 
+    notifyListeners();
   }
 
-  // productExist(ProductModel product) {
-  //   if (_carts.indexWhere((element) => element.product.id == product.id)) {
-  //
-  //   }
-  // }
+  removeCart(int id) {
+    _carts.removeAt(id);
+    notifyListeners();
+  }
+
+  addQuantity (int id) {
+    _carts[id].qty = (_carts[id].qty ?? 0) + 1;
+    notifyListeners();
+  }
+
+  reduceQuantity(int id) {
+    _carts[id].qty = (_carts[id].qty ?? 0) - 1;
+    if(_carts[id].qty == 0){
+      _carts.removeAt(id);
+    }
+    notifyListeners();
+  }
+
+  totalItems() {
+    int total = 0;
+    for(var item in _carts) {
+      total += item.qty!;
+    }
+    return total;
+  }
+
+  totalPrice() {
+    double total = 0;
+    for(var item in _carts) {
+      // total += (item.qty * item.product.price!);
+    }
+    return total;
+  }
+
+  productExist(ProductModel product) {
+    if (_carts.indexWhere((element) => element.product?.id == product.id) == -1 ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
